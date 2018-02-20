c3b8871e3e50d830381e1a8b356f4ab37b35eb05
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-02-19 18:55:45 -0800

    hw2 init

--- hw1_scratch.R
+++ hw1_scratch.R
@@ -4,3 +4,5 @@ solution_guide = 'https://rpubs.com/ppaquay'
 b0 = 50; b1 = 20; b2 = .07; b3 = 35; b4 = .01; b5 = -10
 
 (b0 + b1*3.6 + b2*110 + b3*0 + b4*3.6*110 + b5*3.6*0) * 1000
+
+library(dplyr)
