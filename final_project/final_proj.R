2959a3526a37ff57183833ffa5c9b62c5d482e59
jdubx2 <jesse.weiss.2017@marshall.usc.edu>
2018-04-15 08:17:47 -0700

    decrease smote downsamle

--- final_project/final_proj.R
+++ final_project/final_proj.R
@@ -124,18 +124,18 @@ train_all <- train_all %>%
          ip = factor(ip),
          click_time = factor(click_time))
 
-train1 <- train_all[1:1000000,]
+train1 <- train_all[58316:1000000,]
 test1 <- train_all[1000001:1500000,]
 
 table(train1$is_attributed)
 
 set.seed(100)
-smote_train <- SMOTE(is_attributed ~ ., data = train1, perc.over = 1000)
+smote_train <- SMOTE(is_attributed ~ ., data = train1, perc.over = 1000, perc.under = 1000)
 
 table(smote_train$is_attributed)
 
 #check equivilency
-smote_train %>% 
+train1 %>% 
   dplyr::select(is_attributed, contains('channel'), -channel) %>% 
   gather(var, val, -is_attributed) %>% 
   group_by(var, is_attributed) %>% 
@@ -162,8 +162,8 @@ roc_df %>%
   stat_function(fun = function(x) x)+
   theme_bw()
 
-#write.csv(smote_train, 'train_smote.csv', row.names = F)
+write.csv(smote_train, 'train_smote.csv', row.names = F)
 
-smote <- read.csv('train_smote.csv')
+#smote <- read.csv('train_smote.csv')
 
            
